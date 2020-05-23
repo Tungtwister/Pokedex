440e24474465a1df7b76fe0a9445746444dc7fcf
Austin Tung <austin.tung56@gmail.com>
2020-05-23 14:34:18 -0700

    enter key and not case sensitive

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -3,13 +3,17 @@ function($scope, $http, pokeapi) {
 	$scope.pokeName;
 
 	$scope.pokeSearch = function() {
-		$http.get('https://pokeapi.co/api/v2/pokemon/' + $scope.pokeName)
+		$http.get('https://pokeapi.co/api/v2/pokemon/' + $scope.pokeName.toLowerCase())
 	    .success(function(data) { 
-	    $scope.pokemon = data; 
+		    $scope.pokemon = data;
+		    $http.get($scope.pokemon.species.url)
+		    .success(function(data) {
+		    	$scope.species = data;
+		    }) 
   		})
         .error(function(err) { 
-	    alert("Pokemon does not exist!"); 
-            });
+	    	alert("Pokemon does not exist!"); 
+        });
 	};
 
 	pokeapi.success(function(data) { 
