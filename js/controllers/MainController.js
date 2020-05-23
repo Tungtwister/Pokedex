3793dc38222e72ea1732a63ac6564e1867948627
Austin Tung <austin.tung56@gmail.com>
2020-05-22 23:00:01 -0700

    api call works

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,16 +1,18 @@
-app.controller('MainController', function($scope) {
-	$scope.search = {
-		pokeName: "ditto"
+app.controller('MainController', ['$scope', '$http', 'pokeapi',
+function($scope, $http, pokeapi) {
+	$scope.pokeName;
+
+	$scope.pokeSearch = function() {
+		$http.get('https://pokeapi.co/api/v2/pokemon/' + $scope.pokeName)
+	    .success(function(data) { 
+	    $scope.pokemon = data; 
+  		})
+        .error(function(err) { 
+	    alert("Pokemon does not exist!"); 
+            });
 	};
-	$scope.apiCall = function() {
-		alert("Hello")
-	}
-});
 
-
-app.controller('MainController', ['$scope', 'pokeapi', 
-function($scope, pokeapi) { 
-    pokeapi.success(function(data) { 
-    $scope.pokemon = data; 
-  }); 
+	pokeapi.success(function(data) { 
+	$scope.pokemon = data; 
+	}); 
 }]); 
